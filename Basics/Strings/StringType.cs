e5d85f258bc775bab81e3497c8fff8c4cd4bdcd0
Diego Alexandre <ddsa.1988@outlook.com>
2024-04-29 22:08:41 -0300

    commit

--- Basics/Strings/StringType.cs
+++ Basics/Strings/StringType.cs
@@ -28,8 +28,8 @@ public class StringType {
     }
 
     public static void Concatenation() {
-        const string a = "Diego" + " Alexndre";
-        string b = "10" + 5; //Uses the ToString method to convert a nonstring value
+        const string a = "Diego" + " Alexandre";
+        string b = "10" + 5; //Uses the ToString method to convert a non-string value
 
         Console.WriteLine("\"Diego\" + \" Alexandre\" = " + a);
         Console.WriteLine("string \"10\" + int 5 = " + b);
@@ -44,38 +44,36 @@ public class StringType {
         Console.WriteLine($"{b} == {c} = {b == c}");
         Console.WriteLine();
 
-        string text = @"String.Compare(string1, string2, ignoreCase): 
--1 if string1 comes before string2
- 1 if string2 comes before string1
- 0 if their position is the same (string1 and string2 are identical)";
+        const string text = @"string.Compare(strA, strB, ignoreCase) or string.CompareOrdinal(strA, strB): 
+Less than zero => strA is less than strB
+Greater than zero => strA is greater than strB
+Zero => strA and strB are equal";
 
         Console.WriteLine($"{text}\n");
 
         Console.WriteLine("Char 'T' in ASCII = " + (byte)a[0]);
         Console.WriteLine("Char 't' in ASCII = " + (byte)b[0]);
-        Console.WriteLine($"string.Compare({b}, {a}) = {string.Compare(b, a)}");
-        Console.WriteLine($"string.Compare({a}, {b}) = {string.Compare(a, b)}");
-        Console.WriteLine($"string.Compare({b}, {c}) = {string.Compare(b, c)}");
+        Console.WriteLine($"string.Compare({b}, {a}) = {string.CompareOrdinal(b, a)}");
+        Console.WriteLine($"string.Compare({a}, {b}) = {string.CompareOrdinal(a, b)}");
+        Console.WriteLine($"string.Compare({b}, {c}) = {string.CompareOrdinal(b, c)}");
     }
 
     public static void SortArrayOfStrings() {
         string[] names = ["Rodrigo", "Diego", "Ivanice", "Bruno", "Amanda", "Tainara"];
-        Array.Sort( names );
 
         PrintCollection<string>.Print(names);
 
         for (int i = 0; i < names.Length; i++) {
             for (int j = i + 1; j < names.Length; j++) {
-
                 string actual = names[i];
                 string next = names[j];
 
-                int compareResult = string.Compare(actual, next);
+                int compareResult = string.CompareOrdinal(actual, next);
 
-                if (compareResult == 1) {
-                    names[i] = next;
-                    names[j] = actual;
-                }
+                if (compareResult <= 0) continue;
+
+                names[i] = next;
+                names[j] = actual;
             }
         }
 
