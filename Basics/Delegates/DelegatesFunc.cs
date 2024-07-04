2f68a5094eb6d18dd96db804fc00e3609a652952
Diego Alexandre <ddsa.1988@outlook.com>
2024-07-04 00:30:49 -0300

    commit

--- Basics/Delegates/DelegatesFunc.cs
+++ Basics/Delegates/DelegatesFunc.cs
@@ -2,7 +2,36 @@
 
 public class DelegatesFunc {
     public static void UserMain() {
+        Func<int, int, int>[] operations = [Sum, Sub, Mult, Div];
+
+        Func<int, int, int> calculator = operations[0];
+        Console.WriteLine(calculator(20, 10));
+
+        calculator = operations[1];
+        Console.WriteLine(calculator(20, 10));
+
+        calculator = operations[2];
+        Console.WriteLine(calculator(20, 10));
+
+        calculator = operations[3];
+        Console.WriteLine(calculator(20, 10));
     }
 
-   
+    private static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    private static int Sub(int a, int b) {
+        return a - b;
+    }
+
+    private static int Mult(int a, int b) {
+        return a * b;
+    }
+
+    private static int Div(int a, int b) {
+        if (b == 0) return 0;
+
+        return a / b;
+    }
 }
