61cf09df7a9ba86c11aefdb0411c8d642244c9e5
Diego Alexandre <d.alexandre@acxautomation.com>
2024-07-18 13:53:20 -0300

    commit

--- Basics/Enumeration/EnumerationBasics.cs
+++ Basics/Enumeration/EnumerationBasics.cs
@@ -1,13 +1,78 @@
-﻿namespace Basics.Enumeration;
+﻿using System.Collections;
+
+namespace Basics.Enumeration;
 
 public class EnumerationBasics {
     public static void UserMain() {
-        const string s1 = "Diego";
+        {
+            const string s1 = "Diego";
 
-        using CharEnumerator enumerator = s1.GetEnumerator();
-        while (enumerator.MoveNext()) {
-            char element = enumerator.Current;
-            Console.Write(element + " ");
+            using CharEnumerator enumerator = s1.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                char element = enumerator.Current;
+                Console.Write(element + " ");
+            }
+        }
+
+        Console.WriteLine("\n");
+
+        {
+            People<string> people = new People<string>();
+            people.Add("Diego");
+            people.Add("Amanda");
+            people.Add("Rodrigo");
+
+            while (people.MoveNext()) {
+                Console.WriteLine(people.Current);
+            }
+
+            people.Reset();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++) {
+                people.MoveNext();
+                Console.WriteLine(people.Current);
+            }
+
+            people.Reset();
+
+            Console.WriteLine();
+        }
+    }
+
+    public class People<T> : IEnumerable, IEnumerator {
+        private readonly List<T> list = new List<T>();
+        private int count = -1;
+
+        public void Add(T item) {
+            list.Add(item);
+        }
+
+        public IEnumerator GetEnumerator() {
+            return (IEnumerator)this;
+        }
+
+        public bool MoveNext() {
+            if (count >= Size() - 1) return false;
+
+            count++;
+            return true;
+        }
+
+        public object? Current {
+            get {
+                if (count < 0) return null;
+                return list[count];
+            }
+        }
+
+        public void Reset() {
+            count = -1;
+        }
+
+        public int Size() {
+            return list.Count;
         }
     }
 }
