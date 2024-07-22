f45b306e8a28624b5fd7a419503c262d6d49121e
Diego Alexandre <ddsa.1988@outlook.com>
2024-07-22 00:02:07 -0300

    commit

--- Basics/Iterators/IteratorsBasics.cs
+++ Basics/Iterators/IteratorsBasics.cs
@@ -1,5 +1,19 @@
 namespace Basics.Iterators;
 
 public class IteratorBasics {
-    public static void UserMain() { }
+    public static void UserMain() {
+        foreach (int number in Test()) {
+            Console.Write(number + " ");
+        }
+    }
+
+    private static IEnumerable<int> Test() {
+        int[] numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        int index = 0;
+
+        while (index < numbers.Length) {
+            yield return numbers[index++];
+        }
+    }
 }
--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Basics;
+﻿using Basics.Iterators;
+
+namespace Basics;
 
 public class Program {
     public static void Main(string[] args) {
-        EnumerationBasics.UserMain();
+        IteratorBasics.UserMain();
     }
 }
