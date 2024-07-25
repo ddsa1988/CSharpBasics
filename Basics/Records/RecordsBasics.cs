9429cbcae1a41ef5384763b301e896a87eb74fd8
Diego Alexandre <d.alexandre@acxautomation.com>
2024-07-25 14:22:00 -0300

    commit

--- Basics/Records/RecordsBasics.cs
+++ Basics/Records/RecordsBasics.cs
@@ -1,5 +1,23 @@
 namespace Basics.Records;
 
 public class RecordsBasics {
-    public static void UserMain() { }
+    public static void UserMain() {
+        Points p1 = new Points(10, 20);
+        Points p2 = new Points(10, 20);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p1.Equals(p2));
+
+        Console.WriteLine();
+
+        Console.WriteLine(Points.Equals(p1, p2));
+        Console.WriteLine(Points.ReferenceEquals(p1, p2));
+    }
+
+    private record Points {
+        public double X { get; init; }
+        public double Y { get; init; }
+
+        public Points(double x, double y) => (X, Y) = (x, y);
+    }
 }
---
+++ Basics/Tuples/ComparingTuples.cs
@@ -0,0 +1,23 @@
+﻿namespace Basics.Tuples;
+
+public class ComparingTuples {
+    public static void UserMain() {
+        {
+            (string, int) bob1 = ("Bob", 16);
+            (string, int) bob2 = ("Bob", 16);
+
+            Console.WriteLine(bob1.Equals(bob2));
+            Console.WriteLine(bob1 == bob2);
+        }
+
+        Console.WriteLine();
+
+        {
+            (string, int) bob1 = ("Bob", 17);
+            (string, int) bob2 = ("Bob", 16);
+
+            Console.WriteLine(bob1.Equals(bob2));
+            Console.WriteLine(bob1 == bob2);
+        }
+    }
+}
