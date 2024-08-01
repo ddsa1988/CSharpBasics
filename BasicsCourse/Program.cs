952f3e1bbbc880939389ec0ba0ad1ea51d404ac5
Diego Alexandre <ddsa.1988@outlook.com>
2024-07-31 22:50:51 -0300

    commit

---
+++ BasicsCourse/Program.cs
@@ -0,0 +1,13 @@
+﻿// using System; => This directive enables types within the system namespace to be directly referenced in code.
+
+// namespace BasicsCourse; => A namespace is a hierarchical organizational facility.
+
+namespace BasicsCourse;
+
+// public class Program {} => Class organized under the namespace.
+public class Program {
+    // public static void Main() {} => Application entry point.
+    public static void Main(string[] args) {
+        // Console.WriteLine("Hello World!!!"); => Static class organized under the System namespace.
+    }
+}
