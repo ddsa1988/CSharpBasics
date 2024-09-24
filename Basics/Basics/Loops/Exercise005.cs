c0ba4d966dfc1298ee87923a8f1a3250e2b01833
Diego Alexandre <d.alexandre@acxautomation.com>
2024-09-24 15:10:13 -0300

    commit

--- Basics/Basics/Loops/Exercise005.cs
+++ Basics/Basics/Loops/Exercise005.cs
@@ -26,6 +26,7 @@ public class Exercise005 {
         StringBuilder sb = new StringBuilder();
 
         double auxNumber = number;
+        bool flag = true;
 
         for (int i = 0; i <= numberBits; i++) {
             double bitValue = Math.Pow(2, numberBits - i);
@@ -33,20 +34,11 @@ public class Exercise005 {
             if (auxNumber - bitValue >= 0) {
                 auxNumber -= bitValue;
                 sb.Append('1');
-            } else {
+                flag = false;
+            } else if (!flag) {
                 sb.Append('0');
             }
         }
-
-        Console.WriteLine(sb.Length);
-
-        for (int i = 0; i < sb.Length; i++) {
-            if (sb[i] != '0') break;
-
-            sb[i] = '\0';
-        }
-
-        Console.WriteLine(sb.Length);
 
         Console.WriteLine($"The number {number} in binary is {sb}.");
     }
