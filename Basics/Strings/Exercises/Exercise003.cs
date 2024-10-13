97f1a78e3362d638705381c7c71705f1b0168078
Diego Alexandre <ddsa.1988@outlook.com>
2024-10-13 16:57:59 -0300

    commit

--- Basics/Strings/Exercises/Exercise003.cs
+++ Basics/Strings/Exercises/Exercise003.cs
@@ -4,6 +4,13 @@ public class Exercise003 {
     // Write a program that checks whether the parentheses are placed correctly in an arithmetic expression. Example of expression with
     // correctly placed brackets: ((a+b)/5-d). Example of an incorrect expression: )(a+b)).
     public static void UserMain() {
+        string sample = "((a+b)/5-d)";
 
+        Console.WriteLine(IsParenthesesCorrect(sample));
+    }
+
+    public static bool IsParenthesesCorrect(string expression) {
+
+        return false;
     }
 }
