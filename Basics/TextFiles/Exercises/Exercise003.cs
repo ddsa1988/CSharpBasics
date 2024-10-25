ba7d9755a8ac9204affa1e2ae197f35887785107
Diego Alexandre <d.alexandre@acxautomation.com>
2024-10-25 14:54:16 -0300

    commit

--- Basics/TextFiles/Exercises/Exercise003.cs
+++ Basics/TextFiles/Exercises/Exercise003.cs
@@ -4,7 +4,39 @@ public class Exercise003 {
     // Write a program that reads the contents of a text file and inserts the
     // line numbers at the beginning of each line, then rewrites the file contents.
     public static void UserMain() {
-        
+        char sepChar = Path.DirectorySeparatorChar;
+        string sourceFilePath = $"..{sepChar}..{sepChar}..{sepChar}TextFiles{sepChar}Files{sepChar}Exercise003_File1.txt";
+        string targetFilePath = $"..{sepChar}..{sepChar}..{sepChar}TextFiles{sepChar}Files{sepChar}Exercise003_File2.txt";
+
+        InsertLineNumber(sourceFilePath, targetFilePath);
+    }
+
+    private static void InsertLineNumber(string sourceFilePath, string targetFilePath) {
+        if (!File.Exists(sourceFilePath)) return;
+
+        try {
+            using var reader = new StreamReader(sourceFilePath);
+            using StreamWriter write = File.CreateText(targetFilePath);
+
+            int lineCounter = 0;
+
+            while (!reader.EndOfStream) { 
+                string? line = reader.ReadLine();
+                lineCounter++;
+               
+                if(string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line)) {
+                    write.WriteLine(line);
+                } else {
+                    write.WriteLine(lineCounter + " " + line);
+                }
+            }
+
+
+        } catch (IOException) {
+            Console.Error.WriteLine($"Can't read file '{sourceFilePath} and write file {targetFilePath}.'");
+        } catch (Exception e) { 
+            Console.Error.WriteLine (e.ToString());
+        }
     }
 }
 
---
+++ Basics/TextFiles/Exercises/Exercise004.cs
@@ -0,0 +1,35 @@
+﻿namespace Basics.TextFiles.Exercises;
+
+public class Exercise004 {
+    // Write a program that compares two text files with the same number of rows
+    // line by line and prints the number of equal and the number of different lines.
+    public static void UserMain() {
+        char sepChar = Path.DirectorySeparatorChar;
+        string filePath1 = $"..{sepChar}..{sepChar}..{sepChar}TextFiles{sepChar}Files{sepChar}Exercise004_File1.txt";
+        string filePath2 = $"..{sepChar}..{sepChar}..{sepChar}TextFiles{sepChar}Files{sepChar}Exercise004_File2.txt";
+
+        PrintEqualLines(filePath1, filePath2);
+    }
+
+    private static void PrintEqualLines(string filePath1, string filePath2) {
+        if (!File.Exists(filePath1) || !File.Exists(filePath2)) return;
+
+        try {
+            using var reader1 = new StreamReader(filePath1);
+            using var reader2 = new StreamReader(filePath2);
+
+            while (!reader1.EndOfStream && !reader2.EndOfStream) {
+                string? line1 = reader1.ReadLine();
+                string? line2 = reader2.ReadLine();
+
+                Console.WriteLine($"Line 1 = {line1}");
+                Console.WriteLine($"Line 2 = {line2}");
+            }
+
+        } catch (IOException) {
+            Console.Error.WriteLine($"Can't read file '{filePath1}' and file '{filePath2}'.");
+        } catch (Exception e) {
+            Console.Error.WriteLine(e.ToString());
+        }
+    }
+}
