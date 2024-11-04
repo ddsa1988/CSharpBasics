aa31927421d72fcaaabb29803937691bfbc31824
Diego Alexandre <d.alexandre@acxautomation.com>
2024-11-04 14:46:41 -0300

    commit

--- Basics/LinearDataStructures/Models/SimpleLinkedList.cs
+++ Basics/LinearDataStructures/Models/SimpleLinkedList.cs
@@ -43,9 +43,50 @@ public class SimpleLinkedList<T> {
         return currentNode == null ? default(T) : currentNode.Element;
     }
 
+    public int Remove(T item) {
+        int currentIndex = 0;
+        ListNode<T>? currentNode = head;
+        ListNode<T>? previousNode = null;
+
+        while (currentNode != null) {
+            if (Equals(item, currentNode.Element)) break;
+
+            previousNode = currentNode;
+            currentNode = currentNode?.Next;
+            currentIndex++;
+        }
+
+        if (currentNode != null) {
+            RemoveNode(currentNode, previousNode);
+            return currentIndex;
+        }
+
+        return -1;
+    }
+
+    public int IndexOf(T item) {
+        int currentIndex = 0;
+        ListNode<T>? currentNode = head;
+
+        while (currentNode != null) {
+
+            if (Equals(item, currentNode.Element)) return currentIndex;
+
+            currentNode = currentNode.Next;
+            currentIndex++;
+        }
+
+        return -1;
+    }
+
+    public bool Contains(T item) {
+        int index = IndexOf(item);
+        return index != -1;
+    }
+
     private void RemoveNode(ListNode<T>? node, ListNode<T>? previousNode) {
         Count--;
-        
+
         if (Count == 0) {
             head = null;
             tail = head;
