34d205464d75dda25b9ecefdbca7fcfe9538528e
Diego Alexandre <d.alexandre@acxautomation.com>
2024-11-05 14:34:13 -0300

    commit

---
+++ Basics/LinearDataStructures/Examples/Example003.cs
@@ -0,0 +1,14 @@
+﻿namespace Basics.LinearDataStructures.Examples;
+
+public class Example003 {
+    public static void UserMain() {
+        var list = new List<string>();
+
+        list.Add("Diego");
+        list.Add("Amanda");
+
+        foreach(string value in list) {
+            Console.WriteLine(value);
+        }
+    }
+}
