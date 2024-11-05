051fb38364197d4577671ae96de2e50a89d3ca61
Diego Alexandre <ddsa.1988@outlook.com>
2024-11-04 23:45:59 -0300

    commit

--- Basics/LinearDataStructures/Examples/Example002.cs
+++ Basics/LinearDataStructures/Examples/Example002.cs
@@ -3,11 +3,5 @@ using Basics.LinearDataStructures.Models;
 namespace Basics.LinearDataStructures.Examples;
 
 public class Example002 {
-    public static void UserMain() {
-        var list = new SimpleLinkedList<string>();
-        
-        list.Add("Diego");
-        list.Add("Amanda");
-        list.Add("Amora");
-    }
+    public static void UserMain() { }
 }
