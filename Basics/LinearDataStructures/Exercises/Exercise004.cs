2e6694be245c2ed260900224593e7b23c3775584
Diego Alexandre <d.alexandre@acxautomation.com>
2024-11-09 20:06:18 -0300

    commit

--- Basics/LinearDataStructures/Exercises/Exercise004.cs
+++ Basics/LinearDataStructures/Exercises/Exercise004.cs
@@ -1,14 +1,54 @@
-﻿using static System.Net.Mime.MediaTypeNames;
-
-namespace Basics.LinearDataStructures.Exercises;
+﻿namespace Basics.LinearDataStructures.Exercises;
 
 public class Exercise004 {
     // Write a method that finds the longest subsequence of equal numbers in a given List<int> and
     // returns the result as new List<int>.Write a program to test whether the method works correctly.
     public static void UserMain() {
-        var sourceList = new List<int>() { 0, 1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 7, 7, 8, 9 };
-        var targetList = new List<int>();
+        var sourceList = new List<int>() { 0, 1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 9 };
 
-        Console.WriteLine("Source list: {0}.", string.Join(", ", sourceList));
+        var targetList = LongestSequence(sourceList);
+
+        Console.WriteLine("Source: [{0}].", string.Join(", ", sourceList));
+        Console.WriteLine("Longest equal number sequence: [{0}].", string.Join(", ", targetList));
+    }
+
+    private static List<int> LongestSequence(List<int> source) {
+        var sequence = new List<int>();
+
+        if (source.Count == 0) return sequence;
+
+        int listLength = source.Count;
+        int bestValue = source[0];
+        int bestIndex = 0;
+        int bestLength = 1;
+
+        int value = bestValue;
+        int length = bestLength;
+        int index = bestIndex;
+
+        for (int i = 1; i < listLength; i++) {
+            int actualValue = source[i];
+
+            if (actualValue == value) {
+                length++;
+                continue;
+            }
+
+            if (length > bestLength) {
+                bestValue = value;
+                bestLength = length;
+                bestIndex = index;
+            }
+
+            value = actualValue;
+            length = 1;
+            index = i;
+        }
+
+        for (int i = bestIndex; i < bestIndex + bestLength; i++) {
+            sequence.Add(source[i]);
+        }
+
+        return sequence;
     }
 }
