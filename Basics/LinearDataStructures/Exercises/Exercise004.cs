12ca2590e907bdfdec9a52c3f917d6a5d6b307d3
Diego Alexandre <ddsa.1988@outlook.com>
2024-11-10 22:40:48 -0300

    commit

--- Basics/LinearDataStructures/Exercises/Exercise004.cs
+++ Basics/LinearDataStructures/Exercises/Exercise004.cs
@@ -6,7 +6,7 @@ public class Exercise004 {
     public static void UserMain() {
         var sourceList = new List<int>() { 0, 1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 9 };
 
-        var targetList = LongestSequence(sourceList);
+        List<int> targetList = LongestSequence(sourceList);
 
         Console.WriteLine("Source: [{0}].", string.Join(", ", sourceList));
         Console.WriteLine("Longest equal number sequence: [{0}].", string.Join(", ", targetList));
@@ -18,13 +18,14 @@ public class Exercise004 {
         if (source.Count == 0) return sequence;
 
         int listLength = source.Count;
-        int bestValue = source[0];
-        int bestIndex = 0;
-        int bestLength = 1;
 
-        int value = bestValue;
-        int length = bestLength;
-        int index = bestIndex;
+        int value = source[0];
+        int length = 0;
+        int index = 1;
+
+        // int bestValue = value;
+        int bestIndex = index;
+        int bestLength = length;
 
         for (int i = 1; i < listLength; i++) {
             int actualValue = source[i];
@@ -35,7 +36,7 @@ public class Exercise004 {
             }
 
             if (length > bestLength) {
-                bestValue = value;
+                // bestValue = value;
                 bestLength = length;
                 bestIndex = index;
             }
---
+++ Basics/LinearDataStructures/Exercises/Exercise006.cs
@@ -0,0 +1,9 @@
+namespace Basics.LinearDataStructures.Exercises;
+
+public class Exercise006 {
+    // Write a program that removes from a given sequence all numbers that appear an odd count of times.
+    // Example: array = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2} -> {5, 3, 3, 5}
+    public static void UserMain() {
+        var sourceList = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+    }
+}
