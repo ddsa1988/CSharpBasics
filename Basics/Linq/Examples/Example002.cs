6c613fc768c03fdc4e4e5111b533429ebd575c94
Diego Alexandre <ddsa.1988@outlook.com>
2024-11-21 23:33:27 -0300

    commit

--- Basics/Linq/Examples/Example002.cs
+++ Basics/Linq/Examples/Example002.cs
@@ -12,6 +12,17 @@ public class Example002 {
         Console.WriteLine("ToString => " + myCar);
         Console.WriteLine("HashCode => " + myCar.GetHashCode());
         Console.WriteLine("Equals? => " + myCar.Equals(new { Color = "Red", Brand = "BMW", Speed = 180 }));
-        Console.WriteLine("Type => " + myCar.GetType().ToString());
+        Console.WriteLine("Type => " + myCar.GetType());
+        Console.WriteLine();
+
+        var arr = new[] {
+            new { X = 3, Y = 5 },
+            new { X = 4, Y = 6 },
+            new { X = 5, Y = 7 },
+        };
+
+        foreach (var item in arr) {
+            Console.WriteLine(item);
+        }
     }
 }
